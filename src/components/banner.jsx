9ddac193e6e2a897832d1f9bf7651e385c2174ad
Adam Sinning <asinning25@gmail.com>
2019-06-19 17:01:38 -0300

    css updates

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -6,7 +6,7 @@ class Banner extends Component {
       <div className="banner">
         <div className="banner-content">
           <p className="adam">Adam Sinning</p>
-          <h4>Developer, Designer, Doer</h4>
+          <h4>Developer, Designer, Dreamer</h4>
           <button className="btn btn-primary banner-button">Contact</button>
         </div>
       </div>
