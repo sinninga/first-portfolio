f497905a85b5e2cfa12c892787ad8450b719d18e
Adam Sinning <asinning25@gmail.com>
2019-06-19 20:35:31 -0300

    fonts imported and css updates

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -4,6 +4,7 @@ class Projects extends Component {
   render() {
     return (
       <div className="projects">
+      <br/>
       Projects
       </div>
     )
--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -8,30 +8,30 @@ class Skills extends Component {
           <strong className='skill-title'>Developer</strong>
           <br/>
           <ul className='full-list'>
-            <li className='list'>javascript</li>
-            <li className='list'>css</li>
-            <li className='list'>html5</li>
-            <li className='list'>react</li>
-            <li className='list'>ruby</li>
+            <li className='list'>Javascript</li>
+            <li className='list'>CSS</li>
+            <li className='list'>HTML5</li>
+            <li className='list'>React</li>
+            <li className='list'>Ruby</li>
           </ul>
         </div>
         <div className="skill-card">
           <strong className='skill-title'>Designer</strong>
           <br/>
           <ul className='full-list'>
-            <li className='list'>ui/ux design</li>
-            <li className='list'>wireframing</li>
-            <li className='list'>prototyping</li>
-            <li className='list'>user stories</li>
+            <li className='list'>UI/UX design</li>
+            <li className='list'>Wireframing</li>
+            <li className='list'>Prototyping</li>
+            <li className='list'>User Stories</li>
           </ul>
         </div>
         <div className="skill-card">
           <strong className='skill-title'>Toolkit</strong>
           <br/>
           <ul className='full-list'>
-            <li className='list'>sublime text</li>
-            <li className='list'>chrome developer tools</li>
-            <li className='list'>github</li>
+            <li className='list'>Sublime Text</li>
+            <li className='list'>Chrome Dev Tools</li>
+            <li className='list'>Github</li>
           </ul>
         </div>
       </div>
