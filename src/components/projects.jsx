b128f3d2200b83ed70db7c84cf3b71a942f61734
Adam Sinning <asinning25@gmail.com>
2019-06-20 20:13:34 -0300

    project card css updates

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -3,9 +3,44 @@ import React, { Component } from 'react';
 class Projects extends Component {
   render() {
     return (
-      <div className="projects">
-      <br/>
-      Projects
+      <div className="projects-container">
+      <div className="projects-title">Projects</div>
+        <div className="projects">
+          <div className="row-one-cards">
+            <div className="project-card">
+              <strong className='project-title'>CRUD Demo</strong>
+              <br/>
+              <img className="image" src="/assets/sinai.jpg" alt=""/>
+            </div>
+            <div className="project-card">
+              <strong className='project-title'>Sorting Demo</strong>
+              <br/>
+              <img className="image" src="/assets/sinai.jpg" alt=""/>
+            </div>
+            <div className="project-card">
+              <strong className='project-title'>API Calls</strong>
+              <br/>
+              <img className="image" src="/assets/sinai.jpg" alt=""/>
+            </div>
+          </div>
+          <div className="row-two-cards">
+            <div className="project-card">
+              <strong className='project-title'>Deployment</strong>
+              <br/>
+              <img className="image" src="/assets/sinai.jpg" alt=""/>
+            </div>
+            <div className="project-card">
+              <strong className='project-title'>Redux Demo</strong>
+              <br/>
+              <img className="image" src="/assets/sinai.jpg" alt=""/>
+            </div>
+            <div className="project-card">
+              <strong className='project-title'>Design</strong>
+              <br/>
+              <img className="image" src="/assets/sinai.jpg" alt=""/>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
