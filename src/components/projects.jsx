336675c494ce4f843b733dfa1030f485ce8e432b
Adam Sinning <asinning25@gmail.com>
2019-06-20 20:18:09 -0300

    cards turned into links

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -7,38 +7,38 @@ class Projects extends Component {
       <div className="projects-title">Projects</div>
         <div className="projects">
           <div className="row-one-cards">
-            <div className="project-card">
+            <a href="#" className="project-card">
               <strong className='project-title'>CRUD Demo</strong>
               <br/>
               <img className="image" src="/assets/sinai.jpg" alt=""/>
-            </div>
-            <div className="project-card">
+            </a>
+            <a href="#" className="project-card">
               <strong className='project-title'>Sorting Demo</strong>
               <br/>
               <img className="image" src="/assets/sinai.jpg" alt=""/>
-            </div>
-            <div className="project-card">
+            </a>
+            <a href="#" className="project-card">
               <strong className='project-title'>API Calls</strong>
               <br/>
               <img className="image" src="/assets/sinai.jpg" alt=""/>
-            </div>
+            </a>
           </div>
           <div className="row-two-cards">
-            <div className="project-card">
+            <a href="#" className="project-card">
               <strong className='project-title'>Deployment</strong>
               <br/>
               <img className="image" src="/assets/sinai.jpg" alt=""/>
-            </div>
-            <div className="project-card">
+            </a>
+            <a href="#" className="project-card">
               <strong className='project-title'>Redux Demo</strong>
               <br/>
               <img className="image" src="/assets/sinai.jpg" alt=""/>
-            </div>
-            <div className="project-card">
+            </a>
+            <a href="#" className="project-card">
               <strong className='project-title'>Design</strong>
               <br/>
               <img className="image" src="/assets/sinai.jpg" alt=""/>
-            </div>
+            </a>
           </div>
         </div>
       </div>
