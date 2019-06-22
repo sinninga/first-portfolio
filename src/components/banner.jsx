13a27410e52bd93fce86045c05f69ab54a0556f1
Adam Sinning <asinning25@gmail.com>
2019-06-22 04:00:52 -0300

    links working in banner

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -7,7 +7,9 @@ class Banner extends Component {
         <div className="banner-content">
           <p className="adam">Adam Sinning</p>
           <h4 className="tagline">Full-Stack Developer</h4>
-          <button className="btn btn-primary banner-button">See My Work</button>
+          <a href="#projects-link">
+            <button className="btn btn-primary banner-button">See My Work</button>
+          </a>
         </div>
       </div>
     )
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,6 +4,7 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">Top</a>
         <a className="navbar-brand" href="#skill-link">Skills</a>
         <a className="navbar-brand" href="#projects-link">Projects</a>
         <a className="navbar-brand" href="#about-link">About</a>
