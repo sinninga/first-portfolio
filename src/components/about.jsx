913efca13fca201496b219bcd1baa43a5416ea77
Adam Sinning <asinning25@gmail.com>
2019-06-23 20:11:24 -0300

    about section w/ icons

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -10,6 +10,12 @@ class About extends Component {
         <div className="about-content">
           <div className="about-left">
             <div className="about-content-title">Education</div>
+            <div className="about-left-content">
+              <img src="/assets/cuboulder.png" alt="" className="about-icon"/>
+              <img src="/assets/wagon-red.png" alt="" className="about-icon"/>
+              <img src="/assets/cano.png" alt="" className="about-icon"/>
+              <img src="/assets/sucre.png" alt="" className="about-icon"/>
+            </div>
           </div>
           <div className="about-center">
             <div className="about-content-title">Music</div>
