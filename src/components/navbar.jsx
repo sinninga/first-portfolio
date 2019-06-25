98758ad35514b441045a9584901f9c0b878baa5a
Adam Sinning <asinning25@gmail.com>
2019-06-25 17:06:23 -0300

    navbar icon added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,11 +3,15 @@ import React, { Component } from 'react';
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">Top</a>
+      <nav className="navbar">
         <a className="navbar-brand" href="#skill-link">Skills</a>
         <a className="navbar-brand" href="#projects-link">Projects</a>
         <a className="navbar-brand" href="#about-link">About</a>
+        <div className="nav-home">
+          <a className="navbar-brand" href="#">
+            <img className="nav-img" src="/assets/home2.jpg" alt=""/>
+          </a>
+        </div>
       </nav>
     );
   }
