224dff6fe73d64a4f6a9e9eab25717d2ec57ebf3
Adam Sinning <asinning25@gmail.com>
2019-07-16 02:55:03 -0300

    icon path updates

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -11,15 +11,15 @@ class Skills extends Component {
             <strong className='skill-title'>Developer</strong>
             <br/>
             <ul className='full-list-one'>
-              <li className='list'><img className='icon' src="/assets/photos/html.png"/>  HTML5</li>
-              <li className='list'><img className='icon' src="/assets/photos/css.png"/>  CSS</li>
-              <li className='list'><img className='icon' src="/assets/photos/js.png"/>  Javascript</li>
-              <li className='list'><img className='icon' src="/assets/photos/react.png"/>  React</li>
-              <li className='list'><img className='icon' src="/assets/photos/redux.png"/>  Redux</li>
-              <li className='list'><img className='icon' src="/assets/photos/rails.png"/>  Ruby on Rails</li>
-              <li className='list'><img className='icon' src="/assets/photos/bootstrap.png"/>  Bootstrap</li>
-              <li className='list'><img className='icon' src="/assets/photos/heroku.png"/>  Heroku</li>
-              <li className='list'><img className='icon' src="/assets/photos/postgres.png"/>  Postgres</li>
+              <li className='list'><img className='icon' src="https://sinninga.github.io/first-portfolio/assets/photos/html.png"/>  HTML5</li>
+              <li className='list'><img className='icon' src="https://sinninga.github.io/first-portfolio/assets/photos/css.png"/>  CSS</li>
+              <li className='list'><img className='icon' src="https://sinninga.github.io/first-portfolio/assets/photos/js.png"/>  Javascript</li>
+              <li className='list'><img className='icon' src="https://sinninga.github.io/first-portfolio/assets/photos/react.png"/>  React</li>
+              <li className='list'><img className='icon' src="https://sinninga.github.io/first-portfolio/assets/photos/redux.png"/>  Redux</li>
+              <li className='list'><img className='icon' src="https://sinninga.github.io/first-portfolio/assets/photos/rails.png"/>  Ruby on Rails</li>
+              <li className='list'><img className='icon' src="https://sinninga.github.io/first-portfolio/assets/photos/bootstrap.png"/>  Bootstrap</li>
+              <li className='list'><img className='icon' src="https://sinninga.github.io/first-portfolio/assets/photos/heroku.png"/>  Heroku</li>
+              <li className='list'><img className='icon' src="https://sinninga.github.io/first-portfolio/assets/photos/postgres.png"/>  Postgres</li>
             </ul>
           </div>
           <div className="skill-card">
